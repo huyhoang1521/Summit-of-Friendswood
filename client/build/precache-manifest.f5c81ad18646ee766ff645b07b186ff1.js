14e454df56bdcb41475991387570931d0d08800d
Huy Hoang <hhoang2197@gmail.com>
2021-01-04 20:57:00 -0600

    Added logic to run front/back end concurrently

---
+++ client/build/precache-manifest.f5c81ad18646ee766ff645b07b186ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7ebfc86b03d0aeaf46ed17c4f6cfbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96b8252f7672847d5c2a",
+    "url": "/static/js/2.5e53b680.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5e53b680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec84fca9633d9a57e9f0",
+    "url": "/static/js/main.5b40564d.chunk.js"
+  },
+  {
+    "revision": "68e5bd8e6b9bb29ceb83",
+    "url": "/static/js/runtime-main.fb6dfb87.js"
+  }
+]);
